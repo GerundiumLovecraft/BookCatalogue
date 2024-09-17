754c73c2a675c9014d297bb08f9a44083aad4f4a
Vlad <vladislav.kasperovich02@gmail.com>
2024-09-17 21:24:17 +0100

    Добавьте файлы проекта.

---
+++ BookCatalogue/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalogue.Models
+{
+    public class Book
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [Range(1, 10, ErrorMessage = "Rating must be between 1 and 10")]
+        public int Rating { get; set; }
+
+    }
+}
---
+++ BookCatalogue/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BookCatalogue.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BookCatalogueContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookCatalogueContext") ?? throw new InvalidOperationException("Connection string 'BookCatalogueContext' not found.")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
